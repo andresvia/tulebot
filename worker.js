0db656d5b0a8601b5b08bc449c1c709fae557cb2
andres <andres@pa7.ca>
2015-06-26 04:30:26 -0400

    adding request and webhook on worker

--- worker.js
+++ worker.js
@@ -1,6 +1,7 @@
 
 var redis = require('redis');
 var URI = require('uri-js');
+var request = require('request');
 
 // REDIS_URL => redis://h:password@host:port"
 
@@ -11,18 +12,26 @@ redis_client.auth(redis_uri.userinfo.split(':')[1])
 
 
 redis_client.on('message', function(ch, u) {
-  var update = JSON.parse(u);
-
   // {"update_id":38664384,"message":{"message_id":16,"from":{"id":346904,"first_name":"Andres","last_name":"Villarroel Acosta","username":"andresvia"},"chat":{"id":346904,"first_name":"Andres","last_name":"Villarroel Acosta","username":"andresvia"},"date":1435302373,"text":"yo!"}}
 
-  if (update.message) {
-    if (update.message.text) {
-      var regex = new RegExp(process.env.TRIGGER_TEXT, 'i');
-      if (update.message.text.match(regex)) {
-        console.log(update.message.text);
-      }
-    }
+  var update = JSON.parse(u);
+  if (!update.message) return;
+  if (!update.message.text) return;
+  var regex = new RegExp(process.env.TRIGGER_TEXT, 'i');
+  if (!update.message.text.match(regex)) return;
+  var tg_url = 'https://api.telegram.org/bot' + process.env.TELEGRAM_BOT_KEY + '/sendMessage';
+  var body = {
+    chat_id: update.message.chat.id,
+    text: process.env.BOT_SAY,
+    reply_to_message_id: update.message.message_id
   }
+  var options = {
+    url: tg_url,
+    method: 'POST',
+    json: true,
+    body: body
+   }
+  request.request(options);
 
 });
 
