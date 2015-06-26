baa125d3102a23d3c9903b74635e80cee2107f88
andres <andres@pa7.ca>
2015-06-26 02:32:53 -0400

    add body parser and debug

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.set('trust proxy', true);
-app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
-app.all('*', function(request, response) {
-  console.log(request);
-  response.send();
+app.all('*', function(req, res) {
+  console.log(req.body);
+  res.send();
 });
 
 app.listen(app.get('port'), function() {
