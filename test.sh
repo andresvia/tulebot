6e93f17acebf70b19c6982dae84a11d55f744252
andres <andres.via@gmail.com>
2015-07-23 15:21:39 -0700

    remove test script

--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-redis-cli << EOF
-publish 'zagabot' '{"update_id":38664384,"message":{"message_id":16,"from":{"id":346904,"first_name":"Andres","last_name":"Villarroel Acosta","username":"andresvia"},"chat":{"id":346904,"first_name":"Andres","last_name":"Villarroel Acosta","username":"andresvia"},"date":1435302373,"text":"yo zaga!"}}'
-EOF
